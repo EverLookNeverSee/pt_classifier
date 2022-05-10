1c882797149b9351a2a1dd88330605084ada29ad
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-10 23:57:16 +0430

    Converting request to json dict

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import requests
 # Sending the request
 resp = requests.post("http://localhost:5000/predict",
                      files={"file": open("sample_images/Bentley-Continental-GT.jpg", "rb")})
+# request to dict
+information = resp.json()
+print(f"Class ID: {information['class_id']}, Class Name: {information['class_name']}")
