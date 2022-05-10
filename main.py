cb8bbde6c3b906e2b6e46e57e8d1d114b999f832
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-10 23:56:31 +0430

    Sending request

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import requests
+
+# Sending the request
+resp = requests.post("http://localhost:5000/predict",
+                     files={"file": open("sample_images/Bentley-Continental-GT.jpg", "rb")})
