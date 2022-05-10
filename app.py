0c634590e055eee5b20555ea83f444399c9ba984
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-10 23:08:00 +0430

    Importing relevant packages

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask
+import io
+import json
+from PIL import Image
+from torchvision import models
+from flask import Flask, jsonify, request
+import torchvision.transforms as transforms
+
 
 # Creating app object
 app = Flask(__name__)
