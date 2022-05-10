87b101e3c32ff8133da94ad3d117d0f228774816
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-10 23:36:12 +0430

    Adding app run section

--- app.py
+++ app.py
@@ -44,3 +44,7 @@ def predict():
         img_bytes = file.read()
         class_id, class_name = get_prediction(image_bytes=img_bytes)
         return jsonify({'class_id': class_id, 'class_name': class_name})
+
+
+if __name__ == '__main__':
+    app.run()
